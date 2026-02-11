424858f409b8f6c20e6c53f73033314cd15308c3
Sebastian Graf <sgraf1337@gmail.com>
2026-02-11 09:53:22 +0000

    feat: switching on new do elaborator in Core

--- tests/lean/run/doLogicTests.lean
+++ tests/lean/run/doLogicTests.lean
@@ -13,6 +13,7 @@ open Std.Do
 set_option grind.warning false
 set_option mvcgen.warning false
 set_option warn.sorry false
+set_option backward.do.legacy false
 
 namespace Code
 
@@ -201,7 +202,6 @@ theorem returning_loop_spec :
   case post =>
     split
     · mspec
-      mspec
       intro _ h
       simp at h
       grind
@@ -234,7 +234,6 @@ theorem fib_triple : ⦃⌜True⌝⦄ fib_impl n ⦃⇓ r => ⌜r = fib_spec n�
   mintro _
   if h : n = 0 then simp [h] else
   simp only [h, reduceIte]
-  mspec -- Spec.pure
   mspec Spec.forIn_range (⇓ ⟨xs, a, b⟩ => ⌜a = fib_spec xs.pos ∧ b = fib_spec (xs.pos + 1)⌝) ?step
   case step => intros; mintro _; simp_all
   simp_all [Nat.sub_one_add_one]
@@ -245,15 +244,14 @@ theorem fib_triple_cases : ⦃⌜True⌝⦄ fib_impl n ⦃⇓ r => ⌜r = fib_sp
   intro h
   mintro -
   simp only [fib_impl, h, reduceIte]
-  mspec
   mspec Spec.forIn_range (⇓ ⟨xs, a, b⟩ => ⌜a = fib_spec xs.pos ∧ b = fib_spec (xs.pos + 1)⌝) ?step
-  case step => intros; mintro _; mspec; mspec; simp_all
+  case step => intros; mintro _; mspec; simp_all
   simp_all [Nat.sub_one_add_one]
 
 theorem fib_impl_vcs
     (Q : Nat → PostCond Nat PostShape.pure)
     (I : (n : Nat) → (_ : ¬n = 0) →
-      Invariant [1:n].toList (MProd Nat Nat) PostShape.pure)
+      Invariant [1:n].toList (Prod Nat Nat) PostShape.pure)
     (ret : ⊢ₛ (Q 0).1 0)
     (loop_pre : ∀ n (hn : ¬n = 0), ⊢ₛ (I n hn).1 ⟨⟨[], [1:n].toList, rfl⟩, 0, 1⟩)
     (loop_post : ∀ n (hn : ¬n = 0) r, (I n hn).1 ⟨⟨[1:n].toList, [], by simp⟩, r⟩ ⊢ₛ (Q n).1 r.2)
@@ -266,13 +264,11 @@ theorem fib_impl_vcs
   simp only [fib_impl, hn, ↓reduceIte]
   mstart
   mspec
-  mspec
   case pre => exact loop_pre n hn
   case post.success => mspec; mpure_intro; apply_rules [loop_post]
   case step =>
     intro _ _ _ _ h;
     mintro _;
-    mspec
     mspec
     mpure_intro
     apply_rules [loop_step]
@@ -388,7 +384,7 @@ theorem fib_triple_erase : ⦃⌜True⌝⦄ fib_impl n ⦃⇓ r => ⌜r = fib_sp
 theorem fib_impl_vcs
     (Q : Nat → PostCond Nat PostShape.pure)
     (I : (n : Nat) → (_ : ¬n = 0) →
-      Invariant [1:n].toList (MProd Nat Nat) PostShape.pure)
+      Invariant [1:n].toList (Prod Nat Nat) PostShape.pure)
     (ret : ⊢ₛ (Q 0).1 0)
     (loop_pre : ∀ n (hn : ¬n = 0), ⊢ₛ (I n hn).1 ⟨⟨[], [1:n].toList, rfl⟩, 0, 1⟩)
     (loop_post : ∀ n (hn : ¬n = 0) r, (I n hn).1 ⟨⟨[1:n].toList, [], by simp⟩, r⟩ ⊢ₛ (Q n).1 r.2)
@@ -693,7 +689,7 @@ theorem max_and_sum_spec (xs : Array Nat) :
     ⦃⌜∀ i, (h : i < xs.size) → xs[i] ≥ 0⌝⦄ max_and_sum xs ⦃⇓ (m, s) => ⌜s ≤ m * xs.size⌝⦄ := by
   mvcgen [max_and_sum]
   case inv1 => exact (⇓ ⟨xs, m, s⟩ => ⌜s ≤ m * xs.pos⌝)
-  all_goals simp_all
+  all_goals simp_all +zetaDelta
   · rw [Nat.left_distrib]
     simp +zetaDelta only [Nat.mul_one, Nat.add_le_add_iff_right]
     rename_i h
@@ -861,29 +857,25 @@ theorem fast_expo_correct (x n : Nat) : fast_expo x n = x^n := by
   generalize h : fast_expo x n = r
   apply Id.of_wp_run_eq h
   mvcgen
-  case inv1 => exact ⇓⟨xs, e, x', y⟩ => ⌜x' ^ e * y = x ^ n ∧ e ≤ n - xs.pos⌝
-  all_goals simp_all
-  case vc1 b _ _ _ _ _ _ ih =>
-    obtain ⟨e, y, x'⟩ := b
-    subst_vars
-    grind
-  case vc2 b _ _ _ _ _ _ ih _ =>
-    obtain ⟨e, y, x'⟩ := b
+  case inv1 => exact ⇓⟨xs, x', y, e⟩ => ⌜x' ^ e * y = x ^ n ∧ e ≤ n - xs.pos⌝
+  all_goals simp_all +zetaDelta
+  case vc2 b _ _ _ _ _ _ ih =>
+    obtain ⟨x', y, e⟩ := b
     simp at *
     constructor
     · rw [← Nat.mul_assoc, ← Nat.pow_add_one, ← ih.1]
       have : e - 1 + 1 = e := by grind
       rw [this]
     · grind
-  case vc3 b _ _ _ _ _ _ ih _ =>
-    obtain ⟨e, y, x'⟩ := b
+  case vc3 b _ _ _ _ _ ih _ =>
+    obtain ⟨x', y, e⟩ := b
     simp at *
     constructor
     · rw [← Nat.pow_two, ← Nat.pow_mul]
       grind
     · grind
   case vc5 b ih =>
-    obtain ⟨e, y, x'⟩ := b
+    obtain ⟨x', y, e⟩ := b
     simp at *
     rw [← ih.1, ih.2, Nat.pow_zero, Nat.one_mul]
 
